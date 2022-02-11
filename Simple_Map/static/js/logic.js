dc548f7bf780f6d799cfc72a2400ee1f173dd757
kristithrasher <kristithrasher@yahoo.com>
2022-02-11 17:50:49 -0500

    adding files

---
+++ Simple_Map/static/js/logic.js
@@ -0,0 +1,31 @@
+// which will contain all the JavaScript and Leaflet code to create the map and add data to the map.
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+//setView() method sets the view of the map with a geographical center(lat and long and zoom level(4))
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+//Alternative to using the setView() method to create map object like this
+//This method is useful when we need to add multiple tile layers, or a background image of our map(s), which we will do later in this module.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+// We create the tile layer that will be the background of our map.
+// Leaflet doesn't provide a tile layer. Instead, it offers various tile layer APIs.
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+
+
