769aa57ff53c9b1ca697e98b9f77632c3349440b
kristithrasher <kristithrasher@yahoo.com>
2022-02-11 19:19:15 -0500

    added circle marker to map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,21 +4,20 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 //setView() method sets the view of the map with a geographical center(lat and long and zoom level(4))
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-//Alternative to using the setView() method to create map object like this
-//This method is useful when we need to add multiple tile layers, or a background image of our map(s), which we will do later in this module.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "yellow",
+    fillcolor: '#ffffa1'
+ }).addTo(map);
+ 
 
 // We create the tile layer that will be the background of our map.
 // Leaflet doesn't provide a tile layer. Instead, it offers various tile layer APIs.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
